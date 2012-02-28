66891f037983f433d539dae782d87e4b83801753
Seamus Abshere <seamus@abshere.net>
2012-02-27 20:52:17 -0600

    don't destroy the characteristics ivar while resolving

--- lib/cohort_scope/cohort.rb
+++ lib/cohort_scope/cohort.rb
@@ -2,28 +2,27 @@ module CohortScope
   class Cohort < ::Arel::Nodes::Node
     IMPOSSIBLE = '1 = 2'
 
-    def initialize(active_record, characteristics, minimum_cohort_size)
-      @active_record = active_record
+    def initialize(active_record_relation, characteristics, minimum_cohort_size)
+      @active_record_relation = active_record_relation
       @characteristics = characteristics
       @minimum_cohort_size = minimum_cohort_size
     end
 
     def expr
-      @expr ||= resolve
+      @expr ||= resolve(@characteristics)
     end
     alias :to_sql :expr
 
     private
 
     # Recursively look for a scope that meets the characteristics and is at least <tt>minimum_cohort_size</tt>.
-    def resolve
-      if @characteristics.empty?
+    def resolve(reduced_characteristics)
+      if reduced_characteristics.empty?
         IMPOSSIBLE
-      elsif (current = @active_record.where(CohortScope.conditions_for(@characteristics))).count >= @minimum_cohort_size
+      elsif (current = @active_record_relation.where(CohortScope.conditions_for(reduced_characteristics))).count >= @minimum_cohort_size
         current.constraints.inject(:and).to_sql
       else
-        @characteristics = self.class.reduce_characteristics(@active_record, @characteristics)
-        resolve
+        resolve self.class.reduce_characteristics(@active_record_relation, reduced_characteristics)
       end
     end
   end
